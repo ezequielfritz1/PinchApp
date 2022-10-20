04820f63cd9ca5048ce4cc9ab4bd24ac9e220341
Ezequiel Fritz <ezequiel@spline.design>
2022-10-20 11:11:53 +0200

    Initial commit

---
+++ PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Ezequiel Fritz on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
